fac62e590a81973ea560973365a0a75392b22118
KarakiBusiness <93557122+KarakiBusiness@users.noreply.github.com>
2022-04-26 01:13:00 -0400

    nav bar completed

--- pyramidbuilders/src/main/java/org/launchcode/pyramidbuilders/controllers/HelloController.java
+++ pyramidbuilders/src/main/java/org/launchcode/pyramidbuilders/controllers/HelloController.java
@@ -1,15 +1,27 @@
 package org.launchcode.pyramidbuilders.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping
-    @ResponseBody
-    public String hello(){
-        return "Hello Spring";
+   @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("title", "My Jobs");
+
+        return "index";
     }
+
+
+
+
+
+
+
+
 }
